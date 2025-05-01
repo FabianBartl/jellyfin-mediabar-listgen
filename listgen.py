7273c481f85ee12d867685cba5f2b3ae7b0971b7
Fabian <fabian@informatic-freak.de>
2025-05-01 19:43:07 +0200

    added comments

--- listgen.py
+++ listgen.py
@@ -27,6 +27,7 @@ class Jellyfin:
         self.server_url = server_url
         self.device_id = str(uuid4()).split("-")[0]
         
+        # create cached session with 1 hour lifespan and authorize as user to get a token
         self.session = requests_cache.CachedSession(
             f"{self.logger.name}.cache",
             expire_after=dt.timedelta(hours=1),
@@ -57,6 +58,7 @@ class Jellyfin:
         return re.sub(r"[^\:][\/]\/+", "/", "/".join(urls))
         
     def get_item_metadata(self, item_id: str) -> dict:
+        # request metadata of a single item
         url = self.join_url(self.server_url, "Users", self.user_id, "Items", item_id)
         response = self.session.get(url)
         response.raise_for_status()
@@ -80,74 +82,78 @@ class RangeFilter:
         
         # match interval type: closed, left-open, right-open
         if re.match(any_pattern + separator_pattern + any_pattern, self.range):
-            self.interval_type = "closed"
+            self.__interval_type = "closed"
         elif re.match(separator_pattern + any_pattern, self.range):
-            self.interval_type = "left-open"
+            self.__interval_type = "left-open"
         elif re.match(any_pattern + separator_pattern, self.range):
-            self.interval_type = "right-open"
+            self.__interval_type = "right-open"
         else:
             raise SyntaxError("Expected format: 'any-any' or 'any-' or '-any'")
 
         # match data type: numeric, alphabetic
-        # extract lower and/or upper bound value
+        # and extract lower and/or upper bound values
         lower_bound, upper_bound = self.range.split("-")
-        if self.interval_type == "closed":
+        if self.__interval_type == "closed":
             if re.match(numeric_pattern, lower_bound) and re.match(numeric_pattern, upper_bound):
-                self.data_type = "numeric"
-                self.lower_bound = float(lower_bound)
-                self.upper_bound = float(upper_bound)
+                self.__data_type = "numeric"
+                self.__lower_bound = float(lower_bound)
+                self.__upper_bound = float(upper_bound)
             elif re.match(alphabetic_pattern, lower_bound) and re.match(alphabetic_pattern, upper_bound):
-                self.data_type = "alphabetic"
-                self.lower_bound = str(lower_bound)
-                self.upper_bound = str(upper_bound)
+                self.__data_type = "alphabetic"
+                self.__lower_bound = str(lower_bound)
+                self.__upper_bound = str(upper_bound)
             else:
                 raise SyntaxError("Expected same data type numeric or alphabetic for lower bound and upper bound.")
             # lower bound higher than upper bound is allowed and inverts the condition
-            self.is_inverted = self.lower_bound > self.upper_bound
-        elif self.interval_type == "left-open":
+            self.__is_inverted = self.__lower_bound > self.__upper_bound
+            
+        elif self.__interval_type == "left-open":
             if re.match(numeric_pattern, upper_bound):
-                self.data_type = "numeric"
-                self.upper_bound = float(upper_bound)
+                self.__data_type = "numeric"
+                self.__upper_bound = float(upper_bound)
             elif re.match(alphabetic_pattern, upper_bound):
-                self.data_type = "alphabetic"
-                self.upper_bound = str(upper_bound)
+                self.__data_type = "alphabetic"
+                self.__upper_bound = str(upper_bound)
             else:
                 raise SyntaxError("Expected data type numeric or alphabetic for upper bound.")
-        elif self.interval_type == "right-open":
+            
+        elif self.__interval_type == "right-open":
             if re.match(numeric_pattern, lower_bound):
-                self.data_type = "numeric"
-                self.lower_bound = float(lower_bound)
+                self.__data_type = "numeric"
+                self.__lower_bound = float(lower_bound)
             elif re.match(alphabetic_pattern, lower_bound):
-                self.data_type = "alphabetic"
-                self.lower_bound = str(lower_bound)
+                self.__data_type = "alphabetic"
+                self.__lower_bound = str(lower_bound)
             else:
                 raise SyntaxError("Expected data type numeric or alphabetic for lower bound.")
 
     def is_in_range(self, value: Union[int, float, str]) -> bool:
-        if self.data_type == "numeric":
+        # ensure data type
+        if self.__data_type == "numeric":
             value = float(value)
-        elif self.data_type == "alphabetic":
+        elif self.__data_type == "alphabetic":
             value = str(value)
         else:
-            raise ValueError(f"Unknown data type '{self.data_type}'")
+            raise ValueError(f"Unknown data type '{self.__data_type}'")
         
-        if self.interval_type == "closed":
-            if not self.is_inverted:
-                return self.lower_bound <= value <= self.upper_bound
+        # check if value is in range, depending on the interval type and inversion
+        if self.__interval_type == "closed":
+            if not self.__is_inverted:
+                return self.__lower_bound <= value <= self.__upper_bound
             else:
-                return self.lower_bound <= value >= self.upper_bound
-        elif self.interval_type == "left-open":
-            return value <= self.upper_bound
-        elif self.interval_type == "right-open":
-            return self.lower_bound <= value
-        raise ValueError(f"Unknown interval type '{self.interval_type}'")
+                return self.__lower_bound <= value >= self.__upper_bound
+        elif self.__interval_type == "left-open":
+            return value <= self.__upper_bound
+        elif self.__interval_type == "right-open":
+            return self.__lower_bound <= value
+        raise ValueError(f"Unknown interval type '{self.__interval_type}'")
         
 
 
 class StaticPlaylist:
     def __init__(self, name: str, item_ids: list[str], sort_by: str = "order", sort_ascending: bool = True, sort_strict: bool = False):
         self.name = name
-        self.item_ids = sorted(set(item_ids), key=item_ids.index)
+        self.item_ids = sorted(set(item_ids), key=item_ids.index)       # remove duplicate item ids
         self.sort_by = sort_by
         self.raise_for_unsupported_sort_by()
         self.sort_ascending = sort_ascending
@@ -158,16 +164,17 @@ class StaticPlaylist:
 
     def raise_for_unsupported_sort_by(self) -> None:
         self.__supported_sort_by = {                                                # data types:
-            "order", "random",                                                      #   Any
-            "Name", "OriginalTitle", "SortName",                                    #   str
+            "order", "random",                                                      #   any
+            "Name", "OriginalTitle", "SortName",                                    #   string
             "DateCreated", "PremiereDate",                                          #   datetime
-            "CriticRating", "CommunityRating", "RunTimeTicks", "ProductionYear",    #   int|float
+            "CriticRating", "CommunityRating", "RunTimeTicks", "ProductionYear",    #   number
         }
         if self.sort_by not in self.__supported_sort_by:
             raise ValueError(f"Can't sort by '{self.sort_by}'")
 
     @staticmethod
     def dict_priority_get(data: dict[str, Any], default_value: Any, primary_key: str, *secondary_keys: str) -> Any:
+        # returns value of the first valid dictionary key, else the set default value
         for key in [primary_key, *secondary_keys]:
             value = data.get(key, ...)
             if value is not ...:
@@ -183,8 +190,12 @@ class StaticPlaylist:
                 item_ids = self.item_ids[:]
                 random.shuffle(item_ids)
                 return item_ids
-            
+        
+        # request item metadata from jellyfin one by one
+        # not in a single bulk request, otherwise the cache functionality won't work as good
         items_metadata = [ jellyfin.get_item_metadata(item_id) for item_id in self.item_ids ]
+
+        # define strict sorting function
         match self.sort_by:
             case "Name" | "OriginalTitle" | "SortName":
                 sort_func = lambda metadata: str(metadata.get(self.sort_by, ""))
@@ -194,6 +205,7 @@ class StaticPlaylist:
                 sort_func = lambda metadata: float(metadata.get(self.sort_by, 0))
 
         if not self.sort_strict:
+            # overwrite strict sorting function if there is one
             match self.sort_by:
                 case "Name" | "OriginalTitle" | "SortName":
                     sort_func = lambda metadata: str(self.dict_priority_get(metadata, "", self.sort_by, "SortName", "Name"))
@@ -233,7 +245,8 @@ class DynamicPlaylist:
     def __repr__(self) -> str:
         return f"{__class__.__name__}(name={self.name.__repr__()}, filters={self.filters.__repr__()}, sort_by={self.sort_by.__repr__()}, sort_ascending={self.sort_ascending.__repr__()}, sort_strict={self.sort_strict.__repr__()})"
 
-    def bake(self, jellyfin: Jellyfin) -> StaticPlaylist:
+    def compile(self, jellyfin: Jellyfin) -> StaticPlaylist:
+        # create a static playlist based on a variety of filters
         item_ids = []
         ... #TODO
         new_playlist = StaticPlaylist(self.name, item_ids, self.sort_by, self.sort_ascending)
@@ -269,6 +282,8 @@ class MediaBar:
         with open(filename, "r", encoding="utf-8", errors="replace") as file:
             filedata = yaml.load(file.read(), yaml.Loader)
         
+        # all playlist names are converted to lower case when imported
+        # altough the StaticPlaylist/DynamicPlaylist and Conditional classes don't requiere lower case names
         self.conditionals = self.__parse_selection(filedata["selection"])
         self.playlists = self.__parse_playlists(filedata["playlists"])
 
@@ -312,7 +327,7 @@ class MediaBar:
                     name,
                     items,
                     entry.get("sort_by", "order"),
-                    entry.get("sort_ascending", True)
+                    entry.get("sort_ascending", True),
                     entry.get("sort_strict", False),
                 )
             
@@ -323,6 +338,7 @@ class MediaBar:
         return playlists
     
     def get_selected(self) -> Conditional:
+        # returns first Conditional where all conditions are met, otherwise the last one
         selected = self.conditionals[-1]
         for conditional in self.conditionals:
             if conditional.is_true():
@@ -331,20 +347,23 @@ class MediaBar:
         return selected
     
     def get_playlist(self, name: str) -> Union[StaticPlaylist, DynamicPlaylist]:
+        # returns first Playlist with matching name
         for playlist in self.playlists:
-            if playlist.name == name:
+            if playlist.name.lower() == name.lower():
                 return playlist
         raise KeyError(f"Selected playlist '{name}' not defined.")
     
     def evaluate(self, jellyfin: Jellyfin) -> tuple[str, list[str]]:
+        # evaluates the whole imported config and returns the selected playlist name and sorted playlist item ids
         conditional = self.get_selected()
         playlist = self.get_playlist(conditional.name)
-        static_playlist = playlist.bake(jellyfin) if isinstance(playlist, DynamicPlaylist) else playlist
+        static_playlist = playlist.compile(jellyfin) if isinstance(playlist, DynamicPlaylist) else playlist
         item_ids = static_playlist.sort(jellyfin)
         return conditional.name, item_ids
     
     @staticmethod
     def export_legacy_format(playlist_name: str, item_ids: list[str], filename: Path) -> None:
+        # the name of the playlist, followed by a list of item ids, one per line
         with open(filename, "w", encoding="utf-8") as file:
             file.writelines([playlist_name])
             file.writelines(item_ids)
